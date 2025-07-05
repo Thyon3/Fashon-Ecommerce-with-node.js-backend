cc8d1e8961a5fcb34fc6a8d431138c6ba922a4f9
Thyon3 <asnakemengesha80@gmail.com>
2025-07-05 05:30:58 -0400

    ready server

--- Backend/app.js
+++ Backend/app.js
@@ -1,37 +1,24 @@
-// core  body-parser  morgan
-
-const bodyParser = require("body-parser");
-const cors = requir("cors");
-require("dotenv/config");
 const express = require("express");
-
+const cors = require("cors");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv/config");
+
 const app = express();
 
-//
-
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
 app.use(cors());
-app.options("*", cors());
+app.use(morgan("tiny"));
+
 
 const env = process.env;
-
 const port = env.PORT || 3000;
 const hostName = env.HOST || "localhost";
 
-app.use(express.json());
-
-app.get("/", (req, res, next) => {
-  res.send("hello world");
-});
-app.post("/login", (req, res, next) => {
-  res.send("you are in the login page");
+app.get("/", (req, res) => {
+  res.send("✅ Server is working!");
 });
 
-app.get("/getme", (req, res, next) => {
-  res.send("you are in a getme page welcome back you dickehead");
-});
 app.listen(port, hostName, () => {
-  console.log(`Server is running on http://${hostName}:${port}`);
+  console.log(`🚀 Server is running at http://${hostName}:${port}`);
 });
