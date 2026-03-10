bff57ca8c8363004977751b9e95db34a2f16de4f
Thyon3 <asnakemengesha80@gmail.com>
2026-03-10 05:04:41 -0400

    add security headers

---
+++ Backend/middlewares/security.js
@@ -0,0 +1,27 @@
+const helmet = require('helmet');
+
+class Security {
+  static middleware() {
+    return helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          scriptSrc: ["'self'"],
+          imgSrc: ["'self'", "data:", "https:"],
+        },
+      },
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true
+      }
+    });
+  }
+
+  static configure(app) {
+    app.use(this.middleware());
+  }
+}
+
+module.exports = Security;
